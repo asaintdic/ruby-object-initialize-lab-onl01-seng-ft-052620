0367e9c2c3220adbf71cf6f6367bf05764b2f28c
Alain Saint-Dic <alains214@gmail.com>
2020-06-05 04:44:09 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,18 +3,6 @@ class Dog
     @name = name
     @breed = breed 
   end
-  def name=(name)
-    @name = name
-  end
-  def name
-    @name
-  end
-  def breed=(breed)
-    @breed = breed
-  end
-  def breed 
-    @breed 
-  end 
 end 
 
 fido = Dog.new("Fido", "Pug")
